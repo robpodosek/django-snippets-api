bfc0d0992522e5e0bce99f829c5f693a0d64b6ec
Robert Podosek <rpodosek@gmail.com>
2023-05-08 16:29:11 -0700

    Add throttling, change pagination size.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -74,7 +74,15 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 
